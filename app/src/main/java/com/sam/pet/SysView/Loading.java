8289fcf27117e4a6765ee51bd2747b60f45f6a30
Administrator <wxx90426@163.com>
2016-08-18 10:48:30 +0800

    new class

--- app/src/main/java/com/sam/pet/SysView/Loading.java
+++ app/src/main/java/com/sam/pet/SysView/Loading.java
@@ -2,6 +2,7 @@ package com.sam.pet.SysView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.sam.pet.R;
 
@@ -10,6 +11,7 @@ public class Loading extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_loading);
     }
 }
