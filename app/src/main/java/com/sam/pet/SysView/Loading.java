eb4be77ea8cfaa749d8900973f1a412fdb1fc93c
Administrator <wxx90426@163.com>
2016-08-18 10:23:21 +0800

    new class

---
+++ app/src/main/java/com/sam/pet/SysView/Loading.java
@@ -0,0 +1,15 @@
+package com.sam.pet.SysView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sam.pet.R;
+
+public class Loading extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
